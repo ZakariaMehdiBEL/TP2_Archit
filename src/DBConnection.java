1ff11ca750b56f393ab43c3ed3f616bc121ef58b
ZakariaMehdiBEL <119809638+ZakariaMehdiBEL@users.noreply.github.com>
2022-12-06 11:36:01 +0100

    Update DBConnection.java

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -2,25 +2,34 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBConnection {
-	   
-		String BDD = "nomBD";
-		String url = "jdbc:mysql://localhost:3306/" + BDD;
-		String user = "root";
-		String passwd = "";
-	    private Connection conn;
+import Repos.IdbConnection;
 
+public class DBConnection implements IdbConnection{
 	   
-	    public DBConnection() throws SQLException {
-			conn=DriverManager.getConnection(url, user,passwd);
-		}
+		private String BDD = "nombd";
+		private String url = "jdbc:mysql://localhost:3306/" + BDD;
+		private String user = "root";
+		private String passwd = "";
+	    private Connection conn;
+		private static DBConnection dbc;
+	   
+	    private DBConnection(){}
 
 	    
-	    public Connection getConn() {
+	    public Connection getConn() throws SQLException {
+			conn=DriverManager.getConnection(url, user,passwd);
 			return conn;
 		}
 
+		public static DBConnection getDBConnection(){
+			if(dbc == null){
+			 
+					dbc = new DBConnection();
+		 
+			}
+			return dbc;
+		} 
 
-		
+ 
 	
 }
